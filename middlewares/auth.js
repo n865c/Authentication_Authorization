2fda1729996a306fe6bd3899456994bdb572f51c
Nancy Kumari <nancykumari620@gamil.com>
2024-03-26 21:44:46 +0530

    Authentication and authorization done

---
+++ middlewares/auth.js
@@ -0,0 +1,44 @@
+const jwt=require("jsonwebtoken")
+require('dotenv').config();
+exports.auth=(req,res,next)=>{
+    try{
+    const {token}=req.body;
+    const decode=jwt.verify(token,process.env.jwt_secret);
+    console.log(decode);
+    req.user=decode;
+    next();
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message
+        })
+    }
+}
+exports.isStudent=(req,res,next)=>{
+    try{
+    const {user}=req;
+    if(user.role!=="Student")
+    return res.status(500).json({
+success:false,
+data:"something worng"})
+next();
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message});
+    }
+}
+exports.isAdmin=(req,res,next)=>{
+    try{
+    const {user}=req;
+    if(user.role!=="Admin")
+    return res.status(500).json({
+success:false,
+data:"something worng"})
+next();
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message});
+    }
+}
--- router/router.js
+++ router/router.js
@@ -2,6 +2,17 @@ const express=require('express');
 const router=express.Router();
 const signUp=require("../controllers/signUp");
 const logIn=require("../controllers/logIn");
+const { auth, isStudent, isAdmin } = require('../middlewares/auth');
 router.post("/create",signUp);
 router.post("/login",logIn);
+router.post("/login/student",auth,isStudent,(req,res)=>{
+    res.status(200).json({
+        success:true,
+        mesage:"student logged"})
+});
+router.post("/login/admin",auth,isAdmin,(req,res)=>{
+    res.status(200).json({
+        success:true,
+        mesage:"Admin logged"})
+});
 module.exports=router;
